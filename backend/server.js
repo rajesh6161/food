27d1b74819220c5569504191141bb3726748e44c
rajesh6161 <rajeshranjan2712000@gmail.com>
2021-04-02 10:06:23 +0530

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import path from 'path'
+import path from 'path';
 // import items from './data/items.js';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -17,24 +17,22 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
 app.use('/api/items', itemRoutes);
 app.use('/api/menu', menuRoutes);
 app.use('/api/users', userRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    })
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '/frontend/build', 'index.html'));
+  });
 } else {
-    app.get('/', (req, res) => {
-        res.send('Server Running on port 3001')
-    })
-    
+  app.get('/', (req, res) => {
+    res.send('Server Running on port 3001');
+  });
 }
 
 app.use(notFound);
